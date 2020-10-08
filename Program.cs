fcc0e9e1e45f017259787c03f314ca26355f7298
DaBoop <arti_2002_2011@mail.ru>
2020-10-08 20:05:39 +0300

    ToString

--- Program.cs
+++ Program.cs
@@ -24,7 +24,7 @@ namespace lab3
             private static int carNumber = 0;
 
 
-
+            public override string ToString() => $"{color} {Brand} {Model} ({production_year}) {price} - {registration} ({id})";
             public Car(int id, string brand, string model, int production_year, int price, string registration, string color = "")
             {
                 this.id = id;
@@ -163,8 +163,8 @@ namespace lab3
             Car[] carList = new Car[]
             {
                new Car(1, "Volvo", "A1", 2002, 100, "A1111", "red"),
-               new Car(2, "Lada", "F3", 2020, 33000, "A1112"),
-               new Car(3, "Volvo", "A1", 2003, 110, "A11113"),
+               new Car(2, "Lada", "F3", 2020, 33000, "A1112", "blue"),
+               new Car(3, "Volvo", "A1", 2003, 110, "A11113", "green"),
                new Car(4, "Volvo", "A1", 2005, 120, "A11114"),
                new Car(5, "Audi", "D3", 2006, 1000, "A11115"),
                new Car(6, "Volvo", "A4", 2010, 300, "A11116"),
@@ -187,12 +187,14 @@ namespace lab3
             //Car RandomCar = new Car();
             //Console.WriteLine($"\n{RandomCar.Id}");
             Console.WriteLine(Car.Info);
-            Console.WriteLine($"Равны ли Volvo A1 и Lada F3?: {Car.Equals(carList[0], carList[1])}");
+            Console.WriteLine($"Равны ли {carList[0].ToString()} и {carList[1].ToString()}?: {Car.Equals(carList[0], carList[1])}");
+
 
             var AnonCar = new { model = "Why", brand = "Do", price = 1, registration = "Exist", production_year = 1999, color = "blue" };
 
             Console.Write("\n");
             Console.Write("Press any key to continue . . . ");
+            
             Console.ReadKey(true);
 
         }
